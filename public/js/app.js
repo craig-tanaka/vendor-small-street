1e34e73629ff5e7fd03e55fe03bbfe4ec1bdb7a8
Tanxy Rogue <tanxyrogue@gmail.com>
2020-01-11 07:26:03 +0200

    fixed profile button drawer open and close functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,41 @@
 const sidebar = document.querySelector('#sidebar');
 
-//#region Event Listeners
-if(document.querySelector('#navDrawerBtn')){ //check if navbar on page
-    document.querySelector('#navDrawerBtn').addEventListener('click', (event)=>{
+//#region Event Listeners_____________________________________________________________________________________________
+
+
+// ___________________Sidebar and Drawer________________
+if (document.querySelector('#navDrawerBtn')) { //check if navbar on page
+    document.querySelector('#navDrawerBtn').addEventListener('click', (event) => {
         sidebar.style.display = 'flex';
         document.querySelector('#sidebar-overlay').style.display = 'initial';
     })
 }
-if(document.querySelector('#sidebar-overlay')){
-    document.querySelector('#sidebar-overlay').addEventListener('click', (event)=>{
+if (document.querySelector('#sidebar-overlay')) {
+    document.querySelector('#sidebar-overlay').addEventListener('click', (event) => {
         sidebar.style.display = 'none';
         document.querySelector('#sidebar-overlay').style.display = 'none';
     })
 }
-if(document.querySelector('#close-drawer-btn')){
-    document.querySelector('#close-drawer-btn').addEventListener('click', (event)=>{
+if (document.querySelector('#close-drawer-btn')) {
+    document.querySelector('#close-drawer-btn').addEventListener('click', (event) => {
         sidebar.style.display = 'none';
         document.querySelector('#sidebar-overlay').style.display = 'none';
     })
 }
-    
+
+
+// ___________________PC Nav Profile Button________________
+if (document.querySelector('.pc-nav__account-icon')) {
+    document.querySelector('.pc-nav__account-icon').addEventListener('click', event => {
+        document.querySelector('.pc-nav__account-options').style.display = 'initial';
+        document.querySelector('.account-options-overlay').style.display = 'initial';
+    })
+}
+if (document.querySelector('.account-options-overlay')) {
+    document.querySelector('.account-options-overlay').addEventListener('click', event => {
+        document.querySelector('.pc-nav__account-options').style.display = 'none';
+        document.querySelector('.account-options-overlay').style.display = 'none';
+    })
+}
+
+//#endregion Event Listeners_____________________________________________________________________________________________
